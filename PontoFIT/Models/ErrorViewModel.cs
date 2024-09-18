e4d00033daf05ad51c6c83c23173abfe71364cd0
Igor Lima <62576492+iguulima@users.noreply.github.com>
2024-09-17 23:10:21 -0300

    Codigo base do c#

---
+++ PontoFIT/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace PontoFIT.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
