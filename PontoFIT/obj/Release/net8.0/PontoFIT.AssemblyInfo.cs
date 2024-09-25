49abf25b13f7ce724131bf7fc41be5c8d5f75fca
Igor Lima <62576492+iguulima@users.noreply.github.com>
2024-09-25 15:51:00 -0300

    Header e Navbar no projeto principal

--- PontoFIT/obj/Release/net8.0/PontoFIT.AssemblyInfo.cs
+++ PontoFIT/obj/Release/net8.0/PontoFIT.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PontoFIT")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+018ea6371cfea3bfe4cb542c3c70d530c23f1506")]
 [assembly: System.Reflection.AssemblyProductAttribute("PontoFIT")]
--- PontoFIT/obj/Release/net8.0/PontoFIT.RazorAssemblyInfo.cs
+++ PontoFIT/obj/Release/net8.0/PontoFIT.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("PontoFIT")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+018ea6371cfea3bfe4cb542c3c70d530c23f1506")]
-[assembly: System.Reflection.AssemblyProductAttribute("PontoFIT")]
-[assembly: System.Reflection.AssemblyTitleAttribute("PontoFIT")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
